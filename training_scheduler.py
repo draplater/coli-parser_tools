2fff50e7f617a44335059d3ec3f7a8539f1cff63
Chen Yufei <yufei.chen@pku.edu.cn>
2018-11-01 13:29:05 +0000

    add error handling for debugging

--- training_scheduler.py
+++ training_scheduler.py
@@ -1,10 +1,16 @@
 import importlib
+import sys
+import time
+import traceback
+import weakref
+import code
 from argparse import Namespace
 from collections import OrderedDict
 
 from logging import FileHandler
 
 from multiprocessing import Process, Lock
+from multiprocessing.pool import ThreadPool
 
 import os
 
@@ -56,59 +62,6 @@ def parse_dict_multistage(dep_parser_class, dic, prefix=()):
     return parse_cmd_multistage(dep_parser_class, dict_to_commandline(dic, prefix))
 
 
-class TrainingScheduler(object):
-    """
-    Run multiple instance of trainer.
-    """
-
-    def __init__(self, train_func, dep_parser_class, train=None, dev=None, test=None):
-        self.train = train
-        self.dev = dev
-        self.test = test
-        self.train_func = train_func
-        self.dep_parser_class = dep_parser_class
-        self.all_options = OrderedDict()
-
-    def add_options(self, title, options_dict, outdir_prefix=""):
-        options_dict["title"] = title
-        options_dict["outdir"] = os.path.join(outdir_prefix, "model-" + title)
-        options = parse_dict_multistage(self.dep_parser_class, options_dict, ["train"])
-        self.train_func = options.func
-        self.all_options[title] = options
-
-    def run_parallel(self):
-        if len(self.all_options) == 1:
-            self.run()
-            return
-
-        processes = {}
-        for title, options in self.all_options.items():
-            print("Training " + title)
-            processes[title] = Process(target=self.train_func,
-                                       args=(options, self.train, self.dev, self.test))
-
-        try:
-            for index, process in processes.items():
-                process.start()
-            for index, process in processes.items():
-                process.join()
-        except KeyboardInterrupt:
-            for index, process in processes.items():
-                process.terminate()
-
-    def run(self):
-        for title, options in self.all_options.items():
-            logger.info("Training " + title)
-            ret = self.train_func(options, self.train, self.dev, self.test)
-            for handler in logger.handlers:
-                if isinstance(handler, FileHandler):
-                    logger.removeHandler(handler)
-            logger.info("{} Done! result is {}".format(title, ret))
-
-    def clear(self):
-        self.all_options = OrderedDict()
-
-
 def lazy_run_parser(module_name, class_name, title, options_dict, outdir_prefix,
                     initializer_lock, mode="train", initializer=None):
     if mode == "train":
@@ -119,9 +72,115 @@ def lazy_run_parser(module_name, class_name, title, options_dict, outdir_prefix,
         with initializer_lock:
             initializer(options_dict)
 
-    dep_parser_class = getattr(importlib.import_module(module_name), class_name)
-    options = parse_dict_multistage(dep_parser_class, options_dict, [mode])
-    return options.func(options)
+    ret = None
+    need_reload = False
+    need_console = False
+    cache_keeper = {}
+
+    while ret is None:
+        if need_reload:
+            import gc
+            logger.info("Reloading modules...")
+            project_root = os.path.dirname(__file__)
+            try:
+                # noinspection PyUnboundLocalVariable
+                del dep_parser_class, options
+            except NameError:
+                pass
+            modules = list(sys.modules.items())
+            modules_refs = []
+            module = None
+
+            # unload modules in the project
+            for r_module_name, module in modules:
+                if getattr(module, "__file__", "").startswith(project_root):
+                    if "__main__" not in r_module_name:
+                        modules_refs.append((r_module_name, weakref.ref(module)))
+                        # delete this module instead reload it
+                        # because it's difficult to determine reload order
+                        del sys.modules[r_module_name]
+            del module
+            del modules
+
+            # determine whether the module is recycled
+            gc.collect()
+            for r_module_name, module_ref in modules_refs:
+                if module_ref() is not None:
+                    logger.info("{} is not collected by gc".format(r_module_name))
+
+        if need_console:
+            def exit():
+                choice = input("Type yes to exit and start running task: ")
+                if choice == "yes":
+                    raise SystemExit
+
+            try:
+                try:
+                    import IPython
+                    IPython.embed()
+                except ModuleNotFoundError:
+                    code.interact(local=locals())
+            except SystemExit:
+                pass
+
+        # noinspection PyBroadException
+        try:
+            dep_parser_class = getattr(importlib.import_module(module_name), class_name)
+            options = parse_dict_multistage(dep_parser_class, options_dict, [mode])
+            if options.use_exception_handler:
+                importlib.import_module("common_utils").cache_keeper = cache_keeper
+            ret = options.func(options)
+        except KeyboardInterrupt:
+            # won't happen bacause it's running in a thread
+            raise
+        except Exception:
+            if not options_dict["use-exception-handler"]:
+                raise
+            # handle errors
+            traceback.print_exc()
+            # waiting for stderr flush
+            sys.stderr.flush()
+            time.sleep(3)
+
+            # choose error handling method
+            need_console = False
+            while True:
+                logger.info("PID: {}\n".format(os.getpid()))
+                choice = input("Exception occurred. What do you want to do?\n"
+                               "reload - reload codes and retry\n"
+                               "console-keep - keep exception stack and start a interactive console\n"
+                               "console-reload - clear exception stack, reload, and start a interactive console\n"
+                               "pdb - set trace for pdb \n"
+                               "exit - reraise exception and exit\n"
+                               ">> "
+                               )
+                if choice == "exit":
+                    raise  # exit the program
+                elif choice == "reload":
+                    need_reload = True
+                    break  # restart running
+                elif choice == "console-reload":
+                    need_console = True
+                    need_reload = True
+                    break
+                elif choice == "console-keep":
+                    def exit():
+                        raise SystemExit
+
+                    try:
+                        try:
+                            import IPython
+                            IPython.embed()
+                        except ModuleNotFoundError:
+                            code.interact(local=locals())
+                    except SystemExit:
+                        continue  # goto choice
+                elif choice == "pdb":
+                    import pdb
+                    pdb.set_trace()
+                else:
+                    continue
+    return ret
 
 
 class LazyLoadTrainingScheduler(object):
@@ -167,16 +226,49 @@ class LazyLoadTrainingScheduler(object):
                 process.terminate()
 
     def run(self):
+        pool = ThreadPool(1)
         for (title, outdir_prefix, mode), options_dict in self.all_options_and_outdirs.items():
             logger.info("Training " + title)
             if self.initializer is not None:
                 self.initializer(options_dict)
-            ret = lazy_run_parser(self.module_name, self.class_name, title,
-                                  options_dict, outdir_prefix, None, mode)
+            # run in a thread to handle KeyboardInterrupt
+            result_obj = pool.apply_async(
+                lazy_run_parser,
+                (self.module_name, self.class_name, title,
+                 options_dict, outdir_prefix, None, mode)
+            )
+            ret = None
+            while ret is None:
+                try:
+                    ret = result_obj.get()
+                except KeyboardInterrupt:
+                    # handle keyboard interrupt
+                    answer = None
+                    while answer != "yes" or answer != "no":
+                        try:
+                            answer = input("Really exit ? (yes/no)")
+                        except KeyboardInterrupt:
+                            pass
+                    if answer == "yes":
+                        continue
+                    else:
+                        pool.terminate()
+                        raise
             for handler in logger.handlers:
                 if isinstance(handler, FileHandler):
                     logger.removeHandler(handler)
             logger.info("{} Done! result is {}".format(title, ret))
+        pool.terminate()
 
     def clear(self):
         self.all_options_and_outdirs = OrderedDict()
+
+
+class TrainingScheduler(LazyLoadTrainingScheduler):
+    """
+    Just use it for backward compatibility
+    """
+
+    # noinspection PyInitNewSignature
+    def __init__(self, parser_class, initializer=None):
+        super(TrainingScheduler, self).__init__(parser_class.__module__, parser_class.__name__, initializer)
